e5802249858ea87080296d025c7d8f6c57c3570b
Jack MacQuarrie <jack.macquarrie.2021@uni.strath.ac.uk>
2026-03-26 16:51:26 +0000

    Created green_function.py file and added docstring

---
+++ task2/green_function.py
@@ -0,0 +1,13 @@
+"""
+Parallel Monte Carlo Green's function solver for the 2D Poisson equation.
+
+Computes both the Laplace (boundary) and charge Green's functions at a
+specified interior grid point, with standard error estimates.
+
+Copyright (c) 2026 Jack MacQuarrie
+
+This code is released under the MIT License. See the LICENSE file in the
+repository for details.
+
+Python Version: 3.9.21
+"""
