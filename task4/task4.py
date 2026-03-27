0e0eeee9e8c9233688ea09987d46d486deb1fc35
Jack MacQuarrie <jack.macquarrie.2021@uni.strath.ac.uk>
2026-03-27 19:14:36 +0000

    Created file and added docstring

---
+++ task4/task4.py
@@ -0,0 +1,16 @@
+"""
+Computes electrostatic potentials at three sample points using Green's
+functions obtained in Task 2/3.
+
+The potential is evaluated via superposition of boundary and charge
+contributions for all required boundary condition and charge distribution
+combinations. Monte Carlo uncertainties are propagated from the Green's
+functions to the final results.
+
+Copyright (c) 2026 Jack MacQuarrie
+
+This code is released under the MIT License. See the LICENSE file in the
+repository for details.
+
+Python Version: 3.9.21
+"""
